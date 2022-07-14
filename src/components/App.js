8622707927633b2838ef899e5427e1267cae601e
Ayush Dixit <43095344+dixitsh@users.noreply.github.com>
2022-07-14 07:59:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,9 @@ import React, {Component, useState} from "react";
 
 const App = () => {
   return (
-    <>
     <div>
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
-    
-
-    </>
   )
 }
 
